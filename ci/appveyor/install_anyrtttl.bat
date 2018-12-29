55445c4c3841861d1a7c472c6428897b583e6cd3
Antoine <end2endzone@gmail.com>
2018-12-29 10:09:43 -0500

    Created scripts for installing dependencies.

---
+++ ci/appveyor/install_anyrtttl.bat
@@ -0,0 +1,41 @@
+@echo off
+
+:: Validate appveyor's environment
+if "%APPVEYOR_BUILD_FOLDER%"=="" (
+  echo Please define 'APPVEYOR_BUILD_FOLDER' environment variable.
+  exit /B 1
+)
+
+set GTEST_ROOT=%APPVEYOR_BUILD_FOLDER%\third_parties\googletest\install
+set rapidassist_DIR=%APPVEYOR_BUILD_FOLDER%\third_parties\RapidAssist\install
+
+echo ============================================================================
+echo Cloning AnyRtttl into %APPVEYOR_BUILD_FOLDER%\third_parties\AnyRtttl
+echo ============================================================================
+mkdir %APPVEYOR_BUILD_FOLDER%\third_parties >NUL 2>NUL
+cd %APPVEYOR_BUILD_FOLDER%\third_parties
+git clone "https://github.com/end2endzone/AnyRtttl.git"
+cd AnyRtttl
+echo.
+
+echo Checking out version v2.2.0...
+git checkout 2.2.0
+echo.
+
+echo ============================================================================
+echo Compiling...
+echo ============================================================================
+mkdir build >NUL 2>NUL
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=%rapidassist_DIR% ..
+if %errorlevel% neq 0 exit /b %errorlevel%
+cmake --build . --config %Configuration%
+if %errorlevel% neq 0 exit /b %errorlevel%
+echo.
+
+REM echo ============================================================================
+REM echo Installing into %APPVEYOR_BUILD_FOLDER%\third_parties\AnyRtttl\install
+REM echo ============================================================================
+REM cmake --build . --config %Configuration% --target INSTALL
+REM if %errorlevel% neq 0 exit /b %errorlevel%
+REM echo.
---
+++ ci/appveyor/install_win32arduino.bat
@@ -0,0 +1,41 @@
+@echo off
+
+:: Validate appveyor's environment
+if "%APPVEYOR_BUILD_FOLDER%"=="" (
+  echo Please define 'APPVEYOR_BUILD_FOLDER' environment variable.
+  exit /B 1
+)
+
+set GTEST_ROOT=%APPVEYOR_BUILD_FOLDER%\third_parties\googletest\install
+set rapidassist_DIR=%APPVEYOR_BUILD_FOLDER%\third_parties\RapidAssist\install
+
+echo ============================================================================
+echo Cloning win32Arduino into %APPVEYOR_BUILD_FOLDER%\third_parties\win32Arduino
+echo ============================================================================
+mkdir %APPVEYOR_BUILD_FOLDER%\third_parties >NUL 2>NUL
+cd %APPVEYOR_BUILD_FOLDER%\third_parties
+git clone "https://github.com/end2endzone/win32Arduino.git"
+cd win32Arduino
+echo.
+
+echo Checking out version v2.3.1...
+git checkout 2.3.1
+echo.
+
+echo ============================================================================
+echo Compiling...
+echo ============================================================================
+mkdir build >NUL 2>NUL
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=%rapidassist_DIR% ..
+if %errorlevel% neq 0 exit /b %errorlevel%
+cmake --build . --config %Configuration%
+if %errorlevel% neq 0 exit /b %errorlevel%
+echo.
+
+echo ============================================================================
+echo Installing into %APPVEYOR_BUILD_FOLDER%\third_parties\win32Arduino\install
+echo ============================================================================
+cmake --build . --config %Configuration% --target INSTALL
+if %errorlevel% neq 0 exit /b %errorlevel%
+echo.
