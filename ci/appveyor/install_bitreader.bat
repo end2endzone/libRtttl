fef36f3282a3909d55aa63936df39fe82bc8b031
Antoine <end2endzone@gmail.com>
2019-01-01 16:43:26 -0500

    AnyRtttl and BitReader third parties does not require compilation before usage.

--- ci/appveyor/install_bitreader.bat
+++ ci/appveyor/install_bitreader.bat
@@ -23,16 +23,16 @@ echo Checking out version v1.3.0...
 git checkout 1.3.0
 echo.
 
-echo ============================================================================
-echo Compiling...
-echo ============================================================================
-mkdir build >NUL 2>NUL
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=%INSTALL_LOCATION% ..
-if %errorlevel% neq 0 exit /b %errorlevel%
-cmake --build . --config %Configuration%
-if %errorlevel% neq 0 exit /b %errorlevel%
-echo.
+REM echo ============================================================================
+REM echo Compiling...
+REM echo ============================================================================
+REM mkdir build >NUL 2>NUL
+REM cd build
+REM cmake -DCMAKE_INSTALL_PREFIX=%INSTALL_LOCATION% ..
+REM if %errorlevel% neq 0 exit /b %errorlevel%
+REM cmake --build . --config %Configuration%
+REM if %errorlevel% neq 0 exit /b %errorlevel%
+REM echo.
 
 REM echo ============================================================================
 REM echo Installing into %INSTALL_LOCATION%
