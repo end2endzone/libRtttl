b0633456d9b35aec90f219799d741b5235033a84
Antoine <end2endzone@gmail.com>
2019-01-01 12:11:45 -0500

    Added MIT header to all source files.

--- src/libRtttl/notedelayarray.cpp
+++ src/libRtttl/notedelayarray.cpp
@@ -1,38 +1,26 @@
-//
-//  libRTTTL Library - v1.2 - 05/27/2016
-//  Copyright (C) 2016 Antoine Beauchamp
-//  The code & updates for the library can be found on http://end2endzone.com
-//
-// AUTHOR/LICENSE:
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 3.0 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License (LGPL-3.0) for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-//
-// DISCLAIMER:
-//  This software is furnished "as is", without technical support, and with no 
-//  warranty, express or implied, as to its usefulness for any purpose.
-//
-// PURPOSE:
-//  The library allows one to convert an RTTTL melody to other formats like:
-//  - Smaller RTTTL code
-//  - Arduino code (tone() and delay() function calls)
-//  - Binary RTTTL. See AnyRtttl library at http://end2endzone.com
-//
-//  The library also support converting arduino code (tones) to RTTTL.
-//  
-// HISTORY:
-// 05/27/2016 v1.2 - First public release.
-//
+/**********************************************************************************
+ * MIT License
+ * 
+ * Copyright (c) 2018 Antoine Beauchamp
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *********************************************************************************/
 
 #include "NoteDelayArray.h"
 #include "libRtttl/tone.h"
